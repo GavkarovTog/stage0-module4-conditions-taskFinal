fe68eef1cdd0e7b8bde9537146f52dc07c058e3c
Tog <tog.gavk@gmail.com>
2024-05-26 16:05:14 +0300

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        if (month < 1 || month > 12) {
+            System.out.println("invalid date");
+        } else if (month == 2) {
+            System.out.println(year % 4 != 0 || year % 100 == 0 && year % 400 != 0 ? 28 : 29);
+        } else if (month < 8 && month % 2 == 1) {
+            System.out.println(31);
+        } else if (month < 8 && month % 2 == 0) {
+            System.out.println(30);
+        } else if (month > 7 && month % 2 == 0) {
+            System.out.println(31);
+        } else if (month > 7 && month % 2 == 1) {
+            System.out.println(30);
+        }
     }
 }
